789843f88863fd7499d2a53c2f5f72427b1bf36c
SlimJimDodger <lbyxbe@gmail.com>
2019-10-19 15:32:08 -0600

    dummy skeleton build that loads in ksp

---
+++ src/LaunchFX/postbuild.bat
@@ -0,0 +1,6 @@
+$(ProjectDir)..\..\..\pdb2mdb\pdb2mdb.exe $(TargetPath)
+if not exist "$(SolutionDir)\..\GameData\$(TargetName)" mkdir "$(SolutionDir)\..\GameData\$(TargetName)\"
+if not exist "$(SolutionDir)\..\GameData\$(TargetName)\Plugins" mkdir "$(SolutionDir)\..\GameData\$(TargetName)\Plugins\"
+
+copy /y $(TargetDir)$(TargetName)* $(SolutionDir)\..\GameData\$(TargetName)\Plugins 
+copy /y $(ProjectDir)$(TargetName).version $(SolutionDir)\..\GameData\$(TargetName)\ 
